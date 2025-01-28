da7a89c7080f0f1c2c543bec6136bd93dde584e3
Andrew Rich <104226245+DarkbobFTW@users.noreply.github.com>
2025-01-27 16:35:27 -0800

    1st attempt, modals for cooking page

--- src/components/foodGallery.tsx
+++ src/components/foodGallery.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './foodGallery.css';
 import './../index.css';
+import Modal from 'react-modal';
+import { color, motion } from 'framer-motion';
 
 import f1 from '../assets/food/food1.png';
 import f2 from '../assets/food/food2.png';
@@ -15,23 +17,71 @@ import f10 from '../assets/food/food10.png';
 import f11 from '../assets/food/food11.png';
 
 
+
 const FoodGallery: React.FC = () => {
+    const [isOpen, setIsOpen] = React.useState(false);
+    const openModal = () => setIsOpen(true);
+    const closeModal = () => setIsOpen(false);
+
     return (
-    <div className='hero'>
-        <div className='foodgallery'>
-            <img src={f1} alt='' className='foodimg'></img>
-            <img src={f2} alt='' className='foodimg'></img>
-            <img src={f3} alt='' className='foodimg'></img>
-            <img src={f4} alt='' className='foodimg'></img>
-            <img src={f5} alt='' className='foodimg'></img>
-            <img src={f6} alt='' className='foodimg'></img>
-            <img src={f7} alt='' className='foodimg'></img>
-            <img src={f8} alt='' className='foodimg'></img>
-            <img src={f9} alt='' className='foodimg'></img>
-            <img src={f10} alt='' className='foodimg'></img>
-            <img src={f11} alt='' className='foodimg'></img>
+        <div className='hero'>
+            <div className='foodgallery'>
+                <Modal
+                    isOpen={isOpen}
+                    onRequestClose={closeModal}
+                    contentLabel="Note Modal"
+                    className="note-modal"
+                    overlayClassName="note-overlay"
+                >
+                    <motion.div
+                        initial={{ rotateY: 90, opacity: 0 }}
+                        animate={{ rotateY: 0, opacity: 1 }}
+                        exit={{ rotateY: 90, opacity: 0 }}
+                        transition={{ duration: 0.6 }}
+                        className="note-card"
+                    >
+                        <div className="close-icon" onClick={closeModal}>
+                            ✖
+                        </div>
+                        <h2>Honey Jalepeno Sesame Chicken </h2>
+                        <div className='recipe-grid'>
+                            <div>
+                                <h3>The Chicken</h3>
+                                <p>• 1 1/2 lbs Chicken Thighs</p>
+                                <p>• 2 eggs (Beaten) </p>
+                                <p>• 1 cup cornstarch</p>
+                                <p>• Salt & pepper</p>
+                            </div>
+                            <div>
+                                <h3>The Sauce</h3>
+                                <p>• 4 minced fresh garlic</p>
+                                <p>• 3 Sliced Jalapenos</p>
+                                <p>• 1/4 cup honey</p>
+                                <p>• 1/4 cup soy sauce</p>
+                                <p>• 2 tablespoons ketchup</p>
+                                <p>• 3 tablespoons brown sugar</p>
+                                <p>• 2 tablespoons rice vinegar</p>
+                                <p>• 1 tablespoon toasted sesame oil</p>
+                                <p>• 2 tablespoons sesame seeds</p>
+                                <p>• 2 tablespoons sliced green onions</p>
+                            </div>
+                        </div>
+                    </motion.div>
+                </Modal>
+
+                <img src={f1 }onClick={openModal} alt='' className='foodimg'></img>
+                <img src={f2} alt='' className='foodimg'></img>
+                <img src={f3} alt='' className='foodimg'></img>
+                <img src={f4} alt='' className='foodimg'></img>
+                <img src={f5} alt='' className='foodimg'></img>
+                <img src={f6} alt='' className='foodimg'></img>
+                <img src={f7} alt='' className='foodimg'></img>
+                <img src={f8} alt='' className='foodimg'></img>
+                <img src={f9} alt='' className='foodimg'></img>
+                <img src={f10} alt='' className='foodimg'></img>
+                <img src={f11} alt='' className='foodimg'></img>
+            </div>
         </div>
-    </div>
     );
     
 };
